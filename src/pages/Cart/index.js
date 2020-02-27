fad7099972d26309ad37cbe0c6c5ccae9675f6c4
lucasluz99 <lucasluzthatsme@gmail.com>
2020-02-27 20:23:40 -0300

    Add cart style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,64 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <td>
+            <img
+              src="https://static.netshoes.com.br/produtos/tenis-caminhada-leve-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_zoom4.jpg?ims=120x"
+              alt=""
+            />
+          </td>
+          <td>
+            <strong>Tênis dos fluxo</strong>
+            <span>R$ 129,90</span>
+          </td>
+          <td>
+            <div>
+              <button onClick={() => {}} type="button">
+                <MdRemoveCircleOutline color="#7159c1" size={20} />
+              </button>
+              <input readOnly type="number" value="1" />
+              <button onClick={() => {}} type="button">
+                <MdAddCircleOutline color="#7159c1" size={20} />
+              </button>
+            </div>
+          </td>
+          <td>
+            <strong>R$ 129,90</strong>
+          </td>
+          <td>
+            <button type="button" onClick={() => {}}>
+              <MdDelete color="#7159c1" size={20} />
+            </button>
+          </td>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <Total>
+          <span>Total:</span>
+          <strong>R$ 129,90</strong>
+        </Total>
+        <button type="button">Finalizar pedido</button>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,3 +1,96 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #fff;
+  padding: 30px;
+  border-radius: 4px;
+
+  footer {
+    margin-top: 10px;
+    text-align: right;
+
+    button {
+      background: #7159c1;
+      border: 0;
+      color: #fff;
+      text-transform: uppercase;
+      padding: 10px;
+      border-radius: 4px;
+      font-weight: bold;
+      transition: background 200ms;
+      &:hover {
+        background: ${darken(0.05, '#7159c1')};
+      }
+    }
+
+    div {
+      span {
+        font-weight: bold;
+        font-size: 18px;
+        text-transform: uppercase;
+        margin-right: 10px;
+        color: #999;
+      }
+
+      strong {
+        font-weight: bold;
+        font-size: 27px;
+      }
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+    text-transform: uppercase;
+  }
+
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+
+    button {
+      background: none;
+      border: 0;
+      padding: 6px;
+    }
+  }
+
+  img {
+    max-width: 200px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    display: inline-block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding: 6px;
+      width: 50px;
+      margin: 0 10px;
+    }
+  }
+`;
+
+export const Total = styled.div``;
